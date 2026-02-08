c7e1302ed62e0d24ecfcf47b21d1c4313672d701
Omar-Martinez-F <omartinezfuentes@csumb.edu>
2026-02-08 13:56:49 -0800

    Current test files being made to test db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation(libs.androidx.ui.test.junit4)
     val room_version = "2.8.4"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:${room_version}")
