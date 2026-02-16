bb6c2d21ee6e0f35d90e596e11b99869646f8944
Daniel Everman <daniel@everman.info>
2026-02-15 22:00:19 -0800

    Fixed JokeScreenTest not passing and removed ExampleInstrumentedTest since it's just a default test.

--- app/src/androidTest/java/com/cst338/cst438_p1/JokeScreenTest.kt
+++ app/src/androidTest/java/com/cst338/cst438_p1/JokeScreenTest.kt
@@ -1,18 +1,46 @@
 package com.cst338.cst438_p1
 
+import android.content.Context
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runner.manipulation.Ordering
 
 @RunWith(AndroidJUnit4::class)
 class JokeScreenTest {
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<JokeActivity>()
+    val composeTestRule = createComposeRule()
+
+    private lateinit var db: AppDatabase
+    private lateinit var favoriteDao: FavoriteDao
+    private lateinit var jokeDao: JokeDao
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        favoriteDao = db.favoriteDao()
+        jokeDao = db.jokeDao()
+
+        composeTestRule.setContent {
+            JokeScreen(
+                userId = 1,
+                favoriteDao = favoriteDao,
+                jokeDao = jokeDao,
+                onBack = { }
+            )
+        }
+    }
 
     @Test
     fun jokeScreen_showsButtons() {
